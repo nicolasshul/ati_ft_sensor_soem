53b3b62b0dd80598b8bd50df0d48c587390660b1
nicolasshul <74377515+nicolasshul@users.noreply.github.com>
2026-04-17 11:22:25 -0400

    debugging

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "ethercat.h"
 
@@ -40,7 +41,7 @@ int main(int argc, char *argv[]) {
   char * iface = argv[1];
 
   // Sets up NIC
-  if (ecx_int(&ctx, iface) <= 0) {
+  if (ecx_init(&ctx, iface) <= 0) {
     fprintf(stderr, "Error: ecx_init\n");
     return 1;
   }
@@ -64,7 +65,7 @@ int main(int argc, char *argv[]) {
   }
 
   // Calculates working counter
-  int expectedWKC = ctx.group[0].outputsWKC * 2 + ctx.group[0].inputsWKC;
+  int expectedWKC = ctx.grouplist[0].outputsWKC * 2 + ctx.grouplist[0].inputsWKC;
 
   // Configures EtherCat clock and measures propogation delay of slaves
   ecx_configdc(&ctx);
@@ -85,12 +86,12 @@ int main(int argc, char *argv[]) {
   ati_txpdo_t *in  = (ati_txpdo_t *) ctx.slavelist[1].inputs;
 
   if (out == NULL) {
-    fprintf(stderr, "Slave %d outputs pointer is NULL\n", slave);
+    fprintf(stderr, "Slave 1 outputs pointer is NULL\n");
     return 1;
   }
 
   if (in == NULL) {
-    fprintf(stderr, "Slave %d inputs pointer is NULL\n", slave);
+    fprintf(stderr, "Slave 1 inputs pointer is NULL\n");
     return 1;
   }
 
@@ -99,7 +100,7 @@ int main(int argc, char *argv[]) {
 
 
   // Checking all slaves could enter operational mode
-  chk = 40;
+  int chk = 40;
   do {
     ecx_send_processdata(&ctx);
     ecx_receive_processdata(&ctx, EC_TIMEOUTRET);
@@ -120,7 +121,7 @@ int main(int argc, char *argv[]) {
 
     // If not continually sending process data watchdog will be sent
     ecx_send_processdata(&ctx);
-    wkc = ecx_receive_processdata(&ctx, EC_TIMEOUTRET);
+    int wkc = ecx_receive_processdata(&ctx, EC_TIMEOUTRET);
 
     if (wkc >= expectedWKC) {
       printf("Fx=%d  Fy=%d  Fz=%d  Tx=%d  Ty=%d  Tz=%d\n",
@@ -131,7 +132,7 @@ int main(int argc, char *argv[]) {
       fprintf(stderr, "Bad WKC: got %d expected >= %d\n", wkc, expectedWKC);
 
       ecx_readstate(&ctx);
-      for (int i = 1; i <= *(ctx.slavecount); i++) {
+      for (int i = 1; i <= ctx.slavecount; i++) {
         if (ctx.slavelist[i].state != EC_STATE_OPERATIONAL) {
           fprintf(stderr,
               "Slave %d not OP. state = 0x%02x ALstatus = 0x%04x\n",
